c02ea808f4090e90105910cfbf7c685ab7baa2aa
Marc <markidiaz25@gmail.com>
2022-04-07 19:48:17 +0200

    Estilos de home, contacto, about, login, registro y pagina inicial del admin acabadas

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/loginRegister.css">
+    <link rel="stylesheet" href="./css/loginRegisterContacto.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -30,25 +30,27 @@
         </ul>
     </header>
     <div class="container">
-        <div class="row justify-content-center pt-5 mt-5 m-1">
-            <div class="col-md-6 col-sm-8 col-xl-5 col-lg-5 formulario">
-                <form action="{{url("login-proc")}}" method="post">
+        <div class="row justify-content-center pt-8 mt-5 m-2">
+            <div class="col-md-6 col-sm-8 col-xl-10 col-lg-5 formulario">
+                <form action="" method="post">
                     @csrf
                     <div class="form-group text-center pt-3">
-                        <h1 class="text-light">INICIAR SESIÓN</h1>
+                        <h1 class="text-light">Si tienes dudas no dudes en escribirnos!</h1>
                     </div>
-                    <div class="form-group mx-sm-4 pt-3">
-                        <input type="email" class="form-control" placeholder="Escribe tu Correo" name="email_us">
+                    <div class="form-group mx-sm-4 pb-2">
+                        <input type="text" class="form-control" placeholder="Nombre" name="nombre">
                     </div>
-                    <div class="form-group mx-sm-4 pb-3">
-                        <input type="password" class="form-control" placeholder="Ingrese su Contraseña" name="pass_us">
+                    <div class="form-group mx-sm-4 pb-2">
+                        <input type="password" class="form-control" placeholder="Apellido" name="Apellido">
+                    </div>
+                    <div class="form-group mx-sm-4 pb-2">
+                        <input type="password" class="form-control" placeholder="Correo" name="Correo">
+                    </div>
+                    <div class="form-group mx-sm-4 pb-2">
+                        {{-- <input type="text" class="form-control" placeholder="Mensaje" name="Mensaje"> --}}<textarea class="form-control" placeholder="Mensaje" name="Mensaje" id="Mensaje" cols="30" rows="5.5"></textarea>
                     </div>
                     <div class="form-group mx-sm-4 pb-4">
-                        <input type="submit" class="btn btn-block ingresar" value="INICIAR SESIÓN">
-                    </div>
-                    <div class="form-group mx-sm-4 text-left">
-                        <span class="">Aún no estas registrado?</span>
-                        <span><a href="./views/registro.blade.php" class="olvide1">REGISTRARSE</a></span>
+                        <input type="submit" class="btn btn-block ingresar" value="ENVIAR">
                     </div>
                 </form>
     {{-- <h1>Login</h1>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/loginRegister.css">
+    <link rel="stylesheet" href="./css/loginRegisterContacto.css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -38,10 +38,10 @@
                         <h1 class="text-light">INICIAR SESIÓN</h1>
                     </div>
                     <div class="form-group mx-sm-4 pt-3">
-                        <input type="email" class="form-control" placeholder="Escribe tu Correo" name="email_us">
+                        <input type="email" class="form-control" placeholder="Correo" name="email_us">
                     </div>
                     <div class="form-group mx-sm-4 pb-3">
-                        <input type="password" class="form-control" placeholder="Ingrese su Contraseña" name="pass_us">
+                        <input type="password" class="form-control" placeholder="Contraseña" name="pass_us">
                     </div>
                     <div class="form-group mx-sm-4 pb-4">
                         <input type="submit" class="btn btn-block ingresar" value="INICIAR SESIÓN">
