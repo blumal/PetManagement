bdccd516421ffb55c3e283bc7abbe800dc6e4ec5
100007217 <gomezmonterroso14@gmail.com>
2022-05-17 15:44:23 +0200

    Arreglado editar Perfil

--- public/js/valid_perfil.js
+++ public/js/valid_perfil.js
@@ -1,19 +1,19 @@
 //Validación de datos
-function validatePerfil(){
-    var pass_us = document.getElementById('pass_us').value;
-    var pass_us = document.getElementById('pass_us').value;
+function validatePerfil() {
+    var nombre_us = document.getElementById('nombre_us').value;
+    var apellido1_us = document.getElementById('apellido1_us').value;
     var email_us = document.getElementById('email_us').value;
     var contacto1_tel = document.getElementById('contacto1_tel').value;
     var nombre_di = document.getElementById('nombre_di').value;
     var cp_di = document.getElementById('cp_di').value;
     var numero_di = document.getElementById('numero_di').value;
-    var piso_di = document.getElementById('piso_di').value;
-    var puerta_di = document.getElementById('puerta_di').value;
 
-    if (pass_us == "" || pass_us == "" || email_us == "" || contacto1_tel == "" || nombre_di == "" || cp_di == "" || numero_di == "" || piso_di == "" || puerta_di == "") {
+    if (nombre_us == "" || apellido1_us == "" || email_us == "" || contacto1_tel == "" || nombre_di == "" || cp_di == "" || numero_di == "") {
+        console.log("falso")
         alertify.error('Rellene los campos obligatorios marcados con un *');
         return false;
-    }else{
+    } else {
+        console.log("true")
         return true;
     }
 }
