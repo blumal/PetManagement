96dfb9775e699b99708c590b4974f9f4481c15e7
blum <alfredoblumtorres@gmail.com>
2022-05-30 19:43:25 +0200

    Home improved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,16 +16,60 @@
         <img src="./img/imagenesWeb/home.jpg" class="foto">
     </div>
     <div class="row-c flex">
-        <div class="column-2">
-            <div class="twitter">
-                <a class="twitter-timeline" data-lang="en" data-width="850" data-height="700" data-dnt="true" data-theme="dark" href="https://twitter.com/twitter?ref_src=twsrc%5Etfw">Tweets by Pet Management</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-            </div>
+        <div class="content1 column-2">
+            <h1>Team</h1>
+            </br>
+            <p>PetManagement está compuesto por un equipo multidisciplinar de profesionales que trabajan cada día siendo conscientes de que tu mascota es un miembro más de la familia y merece todo nuestro esfuerzo, cariño y dedicación. </p>
+            </br>
+            <p>L’Hospitalet está compuesto por un equipo multidisciplinar de profesionales que trabajan cada día siendo conscientes de que tu mascota es un miembro más de la familia y merece todo nuestro esfuerzo, cariño y dedicación.</p>
+            <center>
+                <img src="{{asset('img/brainstorm.png')}}" alt="">
+            </center>
         </div>
-
-        <div class="column-2">
-            <div class="contenedor_grafico">
-                <canvas id="chart_visitasxmeses"></canvas>
-            </div>
+        <img src="{{asset('img/clinica.jpg')}}" class="content2 column-2" alt="">
+    </div>
+    <div class="row-c flex">
+        <div class="servicesinfo">
+            <center>
+                <h2>Servicios destacados</h2>
+                </br>
+                <p>En nuestra clínica podrás encontrar una amplia gama de servicios veterinarios destinados a ofrecer una atención integral a tu animal de compañía en cualquiera de las etapas de su vida.</p>
+            </center>
+        </div>
+    </div>
+    <div class="row-c flex">
+        <div class="services-box">
+            <h3>Servicios Veterinarios</h3>
+            </br>
+            <p>Ponemos especial énfasis en la prevención de enfermedades y realizamos las pruebas oportunas para obtener diagnósticos precisos y aplicar los tratamientos más adecuados. </p>
+            <center>
+                <img src="{{asset('img/veterinarian (2).png')}}" alt="">
+            </center>
+        </div>
+        <div class="specialities-box">
+            <h3>Especialidades veterinarias</h3>
+            </br>
+            <p>Abordamos diversas disciplinas de la medicina veterinaria: dermatología, oftalmología, sistema cardiorespiratorio, traumatología, neurología, oncología, y animales exóticos.</p>
+            <center>
+                <img src="{{asset('img/veterinarian (1).png')}}" alt="">
+            </center>
+        </div>
+        <div class="shopinfo-box">
+            <h3>Tienda para mascotas</h3>
+            </br>
+            <p>Una amplia gama de productos te espera en nuestro centro. Desde la mejor oferta alimenticia. hasta cualquier tipo de accesorio para el aseo, el descanso o el ocio.</p>
+            <center>
+                <img src="{{asset('img/pet-shop.png')}}" alt="">
+            </center>
+        </div>
+    </div>
+    <div class="row-c flex">
+        <div class="twitter column-2">
+            <a class="twitter-timeline" data-lang="en" data-width="850" data-height="700" data-dnt="true" data-theme="dark" href="https://twitter.com/twitter?ref_src=twsrc%5Etfw">Tweets by Pet Management</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </div>
+    
+        <div class="contenedor_grafico column-2">
+            <canvas id="chart_visitasxmeses"></canvas>
         </div>
     </div>
     <footer>
