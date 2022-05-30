006a85d8f355554b43819e39c35824dce43d4f8c
100007217 <gomezmonterroso14@gmail.com>
2022-05-30 19:20:11 +0200

    Registro con validacion de DNI

--- public/js/login_registro/valid_registro.js
+++ public/js/login_registro/valid_registro.js
@@ -1,5 +1,9 @@
+const { subtract, toUpper } = require("lodash");
+
 //Validación de datos
 function validateRegistro() {
+
+
     var nombre_us = document.getElementById('nombre_us').value;
     var dni_us = document.getElementById('dni_us').value;
     var apellido1_us = document.getElementById('apellido1_us').value;
@@ -19,7 +23,20 @@ function validateRegistro() {
         console.log("falso")
         return false;
     } else {
-        console.log("verdad")
-        return true;
+
+        if (dni_us.length != 9) {
+            alertify.error('El DNI no es correcto');
+            return false;
+        } else {
+            letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+            dni_us_digi = dni_us.substr(0, 8)
+            resto = dni_us_digi % 23
+
+            if (letras[resto] == dni_us.substr(8).toUpperCase()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
